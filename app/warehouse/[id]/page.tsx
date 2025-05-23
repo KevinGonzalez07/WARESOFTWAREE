d6d49b75221ef98e9fb1c4c29067c660661d5e9c
KevinGonzalez07 <kevinglz564@gmail.com>
2025-05-23 15:17:18 -0500

    aaaddfff

--- app/warehouse/[id]/page.tsx
+++ app/warehouse/[id]/page.tsx
@@ -10,22 +10,25 @@ const spaceMono = Space_Mono({
   subsets: ["latin"],
 });
 
-// Color ID to RGB mapping
 const colorMap = [
-  '', // placeholder for index 0
-  'rgb(93, 120, 219)',  // 1 Blue
-  'rgb(235, 71, 71)',     // 2 Red
-  'rgb(255, 204, 0)',   // 3 Yellow
-  'rgb(153, 102, 51)',  // 4 Brown
-  'rgb(0, 204, 255)',   // 5 Aqua
-  'rgb(40, 167, 69)',   // 6 Green
-  'rgb(255, 145, 68)',   // 7 Orange
-  'rgb(111, 66, 193)',  // 8 Purple
-  'rgb(255, 99, 132)',  // 9 Pink
-  'rgb(108, 117, 125)', // 10 Gray
+  "", // índice 0 no usado
+  "rgb(93, 120, 219)",  // 1 Azul
+  "rgb(235, 71, 71)",   // 2 Rojo
+  "rgb(255, 204, 0)",   // 3 Amarillo
+  "rgb(153, 102, 51)",  // 4 Marrón
+  "rgb(0, 204, 255)",   // 5 Aqua
+  "rgb(40, 167, 69)",   // 6 Verde
+  "rgb(255, 145, 68)",  // 7 Naranja
+  "rgb(111, 66, 193)",  // 8 Púrpura
+  "rgb(255, 99, 132)",  // 9 Rosado
+  "rgb(108, 117, 125)", // 10 Gris
 ];
 
-export default async function WarehousePage({ params }: { params: { id: string } }) {
+export default async function WarehousePage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const id = Number(params.id);
 
   const almacen = await prisma.almacen.findUnique({
@@ -37,7 +40,7 @@ export default async function WarehousePage({ params }: { params: { id: string }
     },
   });
 
-  if (!almacen) notFound();
+  if (!almacen) return notFound();
 
   const backgroundColor = colorMap[almacen.color] || "white";
 
@@ -53,20 +56,18 @@ export default async function WarehousePage({ params }: { params: { id: string }
           <UserState />
         </header>
 
-              <div className="flex-1 overflow-auto flex justify-center items-start p-10">
-                <div
-                  style={{ backgroundColor }}
-                  className="rounded-3xl shadow-xl p-8"
-                >
-                  <WarehouseView
-                      nombreAlmacen={almacen.nombre}
-                      descripcionAlmacen={almacen.descripcion}
-                      productos={almacen.productos}
-                    />
-
-                </div>
-              </div>
-
+        <div className="flex-1 overflow-auto flex justify-center items-start p-10">
+          <div
+            style={{ backgroundColor }}
+            className="rounded-3xl shadow-xl p-8"
+          >
+            <WarehouseView
+              nombreAlmacen={almacen.nombre}
+              descripcionAlmacen={almacen.descripcion}
+              productos={almacen.productos}
+            />
+          </div>
+        </div>
       </div>
     </main>
   );
